39eef028bfe140077f0a6d5657c2bfb1709beb12
jascolil01 <jayasco246@gmail.com>
2023-03-24 15:18:49 -0400

    added work to db index and removed plant.js

---
+++ db/index.js
@@ -0,0 +1,14 @@
+const mongoose = require(`mongoose`)
+require("dotenv").config()
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connection Established to Mongo")
+  }).catch(e => {
+    console.log("Connection error", e.message)
+  })
+
+const db = mongoose.connection
+
+module.exports = db
