5e1a33dba8098d2754a38b1ec7d1c2793eca7faf
jascolil01 <jayasco246@gmail.com>
2023-03-24 16:17:46 -0400

    added create and read route for user

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const { Router } = require('express');
+const controllers = require('../controllers')
 const router = Router();
 
 router.get('/', (req, res) => res.send('This is root!'))
+
+router.post('/users', controllers.createUser)
+router.get('/users', controllers.getAllUsers)
 
 module.exports = router;
