957eff3bfdbbfacbcb46f55b311fa9cf60fa5a21
jascolil01 <jayasco246@gmail.com>
2023-03-25 09:45:53 -0400

    added C,R,U to posts

--- controllers/index.js
+++ controllers/index.js
@@ -104,6 +104,45 @@ const deleteTodo = async (req, res) => {
     return res.status(500).send(error.message);
   }
 }
+const createPost = async (req, res) => {
+  try {
+    const post = await new Post(req.body)
+    await post.save()
+    return res.status(201).json({
+      post,
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+    return res.status(200).json({ posts })
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+}
+const getPostById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findById(id)
+    if (post) {
+      return res.status(200).json({ post });
+    }
+    return res.status(404).send('Post with the specified ID does not exists');
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+}
+const updatePost = async (req, res) => {
+  try {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    res.status(200).json(post)
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+}
 
 module.exports = {
   createUser,
@@ -114,5 +153,9 @@ module.exports = {
   createToDo,
   getTodoByUserId,
   updateTodo,
-  deleteTodo
+  deleteTodo,
+  createPost,
+  getAllPosts,
+  getPostById,
+  updatePost
 }
--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,9 @@ router.get('/todo/by_userId/:id', controllers.getTodoByUserId)
 router.put('/todo/update_todo/:id', controllers.updateTodo)
 router.delete('/todo/delete_todo/:id', controllers.deleteTodo)
 
+router.post('/posts', controllers.createPost)
+router.get('/posts', controllers.getAllPosts)
+router.get('/posts/by_id/:id', controllers.getPostById)
+router.put('/posts/update_post/:id', controllers.updatePost)
+
 module.exports = router;
