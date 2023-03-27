efe32ecc4218f377c213ca4e1df8a7b88f90ffc7
jascolil01 <jayasco246@gmail.com>
2023-03-27 09:40:23 -0400

    installed vue-router

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>Hi</h1>
+  <div>
+    <h1 class="About">Hello There</h1>
+    <h2>My name is Joshua Ascolillo, this is my Capstone project for General Assembly</h2>
+    <p>Ever since I started this program a couple of months ago I have grown my passion for technology and programming</p>
+  </div>
 </template>
 
 <script>
