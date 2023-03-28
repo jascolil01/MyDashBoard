7e7897109c28428875fcd199d4338f632996d51a
jascolil01 <jayasco246@gmail.com>
2023-03-28 13:16:15 -0400

    made buttons clickable

--- client/src/components/CalculatorPage.vue
+++ client/src/components/CalculatorPage.vue
@@ -1,5 +1,34 @@
 <template>
-  <div></div>
+  <input :value="''" />
+  <!-- <div class="r1">
+    <div>C</div>
+    <div>+/-</div>
+    <div>%</div>
+    <div>÷</div>
+  </div> -->
+  <div class="r2">
+    <div @click="onClick(7)">7</div>
+    <div @click="onClick(8)">8</div>
+    <div @click="onClick(9)">9</div>
+    <div>*</div>
+  </div>
+  <div class="r3">
+    <div @click="onClick(4)">4</div>
+    <div @click="onClick(5)">5</div>
+    <div @click="onClick(6)">6</div>
+    <div>-</div>
+  </div>
+  <div class="r4">
+    <div @click="onClick(1)">1</div>
+    <div @click="onClick(2)">2</div>
+    <div @click="onClick(3)">3</div>
+    <div>+</div>
+  </div>
+  <div class="r5">
+    <div @click="onClick(0)">0</div>
+    <div>.</div>
+    <div @click="handleSubmit()">=</div>
+  </div>
 </template>
 
 <script>
@@ -11,8 +40,18 @@ export default {
 
   },
   data: () => ({
-    test: []
-  })
+    value1: 0,
+    value2: 0
+  }),
+  methods: {
+    onClick(x) {
+      this.value1 = x
+      console.log(this.value1)
+    },
+    handleSubmit() {
+      console.log(this.value1 + this.value2)
+    }
+  }
 }
 </script>
 
