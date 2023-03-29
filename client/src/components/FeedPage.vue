83de76baaa0f282066a31c8bcfb538bbf9166bc1
jascolil01 <jayasco246@gmail.com>
2023-03-29 12:14:45 -0400

    added feed functionality

--- client/src/components/FeedPage.vue
+++ client/src/components/FeedPage.vue
@@ -1,14 +1,31 @@
 <template>
-  <h1>Hi</h1>
+  <div v-for="post in postData" :key="post._id">
+    <h1>{{ post.name }}</h1>
+    <p>{{ post.content }}</p>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { BASE_URL } from '@/globals';
 
 
 export default {
   name: 'FeedPage',
   components: {
 
+  },
+  data: () => ({
+    postData: {}
+  }),
+  mounted: async function () {
+    await this.getPosts()
+  },
+  methods: {
+    async getPosts() {
+      const res = await axios.get(`${BASE_URL}posts`)
+      this.postData = res.data.posts
+    }
   }
 }
 </script>
