96018f87d919d2a3f6b6c20d7ce086d69e79fad5
jascolil01 <jayasco246@gmail.com>
2023-04-06 12:27:04 -0400

    fixed register page layout

--- client/src/components/RegisterPerson.vue
+++ client/src/components/RegisterPerson.vue
@@ -1,17 +1,30 @@
 <template>
   <h1>Register your user below</h1>
-  <form @submit="handleSubmit">
-    <h1>Username</h1>
-    <input class='input' :value="this.username" @input="$emit(this.username = $event.target.value)" required />
-    <h1>Name</h1>
-    <input class='input' :value="this.name" @input="$emit(this.name = $event.target.value)" required />
-    <h1>Email</h1>
-    <input class='input' :value="this.email" @input="$emit(this.email = $event.target.value)" required />
-    <h1>Salary</h1>
-    <input class='input' type="number" :value="this.salary" @input="$emit(this.salary = $event.target.value)" required />
-    <h1>Password</h1>
-    <input class='input' :value="this.password" @input="$emit(this.password = $event.target.value)" required />
-    <button type="submit">Click to Register</button>
+  <form class="register" @submit="handleSubmit">
+    <div class="inputs">
+      <h1>Username</h1>
+      <input class='input' :value="this.username" @input="$emit(this.username = $event.target.value)" required />
+    </div>
+    <div class="inputs">
+      <h1>Name</h1>
+      <input class='input' :value="this.name" @input="$emit(this.name = $event.target.value)" required />
+    </div>
+    <div class="inputs">
+      <h1>Email</h1>
+      <input class='input' :value="this.email" @input="$emit(this.email = $event.target.value)" required />
+    </div>
+    <div class="inputs">
+      <h1>Salary</h1>
+      <input class='input' type="number" :value="this.salary" @input="$emit(this.salary = $event.target.value)"
+        required />
+    </div>
+    <div class="inputs">
+      <h1>Password</h1>
+      <input class='input' :value="this.password" @input="$emit(this.password = $event.target.value)" required />
+    </div>
+
+    <button class="button" type="submit">Click to Register</button>
+
   </form>
 </template>
 
@@ -51,4 +64,17 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.register {
+  display: flex;
+  flex-flow: column;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.inputs {
+  width: fit-content;
+  height: fit-content;
+  padding: 10px;
+}
+</style>
